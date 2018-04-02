ab4649338110eab13097121fc21a4e0bb7e3cb3d
Richard Kim <richeekim@gmail.com>
2018-04-02 03:36:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
+  reversed = ""
   newsentence = sentence.split
   newsentence.map do |x|
      x.reverse!
    end
-  return newsentence <<
+  return newsentence << reversed
 end
